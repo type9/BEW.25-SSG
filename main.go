25528f8fa0fc5e901c821a1902048028db549fba
Gabriel Bernard Lee <gabriel@Gabriels-MacBook-Pro.local>
2021-01-28 14:19:18 +0800

    Added boilerplate

--- main.go
+++ main.go
@@ -1,4 +1,24 @@
 package main
 
+import (
+	"html/template"
+	"os"
+)
+
+type entry struct {
+	Name string
+	Done bool
+}
+
+type ToDo struct {
+	User string
+	List []entry
+}
+
 func main() {
+	t := template.Must(template.New("template.tmpl").ParseFiles("new.html"))
+	err = t.Execute(os.Stdout, todos)
+	if err != nil {
+		panic(err)
+	}
 }
